c9aa308212f6f49f401e37472091ce7c6352e341
13574798@qq.com <13574798@qq.com>
2024-05-04 20:03:51 +0800

    o

--- src/main.rs
+++ src/main.rs
@@ -146,6 +146,26 @@ fn main() {
     test_callonce();
     test_communication();
     test_continue_rec();
+    test_congest();
+}
+fn test_congest(){
+
+    let (send, recv) = std::sync::mpsc::channel();
+    let num_threads = 3;
+    for i in 0..num_threads {
+        let thread_send = send.clone();
+        thread::spawn(move || {
+            thread_send.send(i).unwrap();
+            println!("thread {:?} finished", i);
+        });
+    }
+
+    // 在这里drop send...
+    drop(send);
+    for x in recv {
+        println!("Got: {}", x);
+    }
+    println!("finished iterating");
 }
 fn test_continue_rec(){
     let (tx, rx) = std::sync::mpsc::channel();
